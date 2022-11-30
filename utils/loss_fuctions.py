584a14f8d11cdf29b643759f29835ea5989aacd9
ray <ruizhe.li@nottingham.ac.uk>
2022-11-30 19:35:31 +0000

    Add MSE NCC Gradient loss

--- utils/loss_fuctions.py
+++ utils/loss_fuctions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -79,3 +80,105 @@ class SoftDice():
         if self.ignore_background:
             soft_dice = soft_dice[:, 1:]
         return 1 - soft_dice.mean()
+
+class MSE:
+    def __call__(self, data_dict):
+        pred = data_dict['logits']
+        target = data_dict['target']
+        mse = F.mse_loss(pred, target)
+        return mse
+
+class NCC:
+    """
+    Local (over window) normalized cross correlation loss.
+    """
+
+    def __init__(self, win=None, eps=1e-5):
+        self.win = win
+        self.eps = eps
+
+    def __call__(self, data_dict):
+
+        Ii = data_dict['target']
+        Ji = data_dict['logits']
+
+        # get dimension of volume
+        # assumes Ii, Ji are sized [batch_size, *vol_shape, nb_feats]
+        ndims = len(list(Ii.size())) - 2
+        assert ndims in [1, 2, 3], "volumes should be 1 to 3 dimensions. found: %d" % ndims
+
+        # set window size
+        win = [9] * ndims if self.win is None else [self.win] * ndims
+
+        # compute filters
+        sum_filt = torch.ones([1, 1, *win], device=Ii.device, requires_grad=False)
+
+        pad_no = math.floor(win[0] / 2)
+
+        if ndims == 1:
+            stride = (1)
+            padding = (pad_no)
+        elif ndims == 2:
+            stride = (1, 1)
+            padding = (pad_no, pad_no)
+        else:
+            stride = (1, 1, 1)
+            padding = (pad_no, pad_no, pad_no)
+
+        # get convolution function
+        conv_fn = getattr(F, 'conv%dd' % ndims)
+
+        # compute CC squares
+        I2 = Ii * Ii
+        J2 = Ji * Ji
+        IJ = Ii * Ji
+
+        I_sum = conv_fn(Ii, sum_filt, stride=stride, padding=padding)
+        J_sum = conv_fn(Ji, sum_filt, stride=stride, padding=padding)
+        I2_sum = conv_fn(I2, sum_filt, stride=stride, padding=padding)
+        J2_sum = conv_fn(J2, sum_filt, stride=stride, padding=padding)
+        IJ_sum = conv_fn(IJ, sum_filt, stride=stride, padding=padding)
+
+        win_size = np.prod(win)
+        u_I = I_sum / win_size
+        u_J = J_sum / win_size
+
+        cross = IJ_sum - u_J * I_sum - u_I * J_sum + u_I * u_J * win_size
+        I_var = I2_sum - 2 * u_I * I_sum + u_I * u_I * win_size
+        J_var = J2_sum - 2 * u_J * J_sum + u_J * u_J * win_size
+
+        cc = cross * cross / (I_var * J_var + self.eps)
+
+        return -torch.mean(cc)
+
+
+class Grad:
+    """
+    N-D gradient loss.
+    """
+
+    def __init__(self, penalty='l1'):
+        self.penalty = penalty
+
+    def __call__(self, data_dict):
+        logits = data_dict['logits']
+        ndims = len(list(logits.size())) - 2
+        df = [None] * ndims
+        for i in range(ndims):
+            d = i + 2
+            # permute dimensions to put the ith dimension first
+            r = [d, *range(d), *range(d+1, ndims+2)]
+            li = torch.permute(logits, r)
+            dfi = li[1:, ...] - li[:-1, ...]
+
+            # permute back
+            r = [*range(1, d + 1), 0, *range(d + 1, ndims + 2)]
+            dfi = torch.permute(dfi, r)
+            df[i] = torch.abs(dfi)
+
+        if self.penalty == 'l2':
+            df = [f * f for f in df]
+
+        grad = torch.stack([torch.mean(f) for f in df]).mean()
+
+        return grad
